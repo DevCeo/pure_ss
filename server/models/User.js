b6b0b878edd1881faf0955f8a4a3c3c00d9aa5d4
JungSoochang <tnckddl2468@gmail.com>
2022-01-22 00:26:07 +0900

    login 기능 + token 생성

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const e = require("express");
 //saltRounds는 salt가 몇글자인지 정한다.
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -56,6 +57,26 @@ userSchema.pre("save", function (next) {
     next();
   }
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword랑 암호화된 비밀번호랑 같은지 확인
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  //jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 // 스키마를 모델로 감싼다.
 const User = mongoose.model("User", userSchema);
